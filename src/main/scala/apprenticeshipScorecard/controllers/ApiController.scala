a3ddae862dd8099ccd62f4d5a01c5eb1ab7720fb
Doug Clinton <doug@wellfactored.com>
2016-04-23 11:54:19 +0100

    Added logging

--- src/main/scala/apprenticeshipScorecard/controllers/ApiController.scala
+++ src/main/scala/apprenticeshipScorecard/controllers/ApiController.scala
@@ -3,6 +3,7 @@ package apprenticeshipScorecard.controllers
 import javax.inject.Inject
 
 import apprenticeshipScorecard.tools.TSVLoader
+import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
@@ -11,7 +12,9 @@ import scala.concurrent.ExecutionContext
 class ApiController @Inject()(implicit ec: ExecutionContext) extends Controller {
 
   def providers = Action {
-    Ok(Json.toJson(TSVLoader.dataStore.providers.values))
+    val json = Json.toJson(TSVLoader.dataStore.providers.values)
+    Logger.debug("built json for providers")
+    Ok(json)
   }
 
   def apprenticeships = Action {
