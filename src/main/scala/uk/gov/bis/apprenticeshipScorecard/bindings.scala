7cd8d5af080222cb5a84e4794d2faa096fa5e04e
Doug Clinton <doug@wellfactored.com>
2016-07-13 12:56:37 +0100

    Removed some duplicated code in bindings.scala

--- src/main/scala/uk/gov/bis/apprenticeshipScorecard/bindings.scala
+++ src/main/scala/uk/gov/bis/apprenticeshipScorecard/bindings.scala
@@ -7,26 +7,21 @@ import scala.util.Try
 object bindings {
 
   implicit val bdBinding = new PathBindable[BigDecimal] {
-    override def bind(key: String, value: String): Either[String, BigDecimal] = {
-      Try(BigDecimal(value)).toOption match {
-        case Some(bd) => Right(bd)
-        case None => Left(s"could not convert parameter $key to a number")
-      }
-    }
+    override def bind(key: String, value: String): Either[String, BigDecimal] = extractBigDecimal(key, value)
 
     override def unbind(key: String, value: BigDecimal): String = value.toString()
   }
 
   implicit val bdQBinding = new QueryStringBindable[BigDecimal] {
-    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, BigDecimal]] = {
-      params.get(key).flatMap(_.headOption).map { value =>
-        Try(BigDecimal(value)).toOption match {
-          case Some(bd) => Right(bd)
-          case None => Left(s"could not convert parameter $key to a number")
-        }
-      }
-    }
+    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, BigDecimal]] =
+      params.get(key).flatMap(_.headOption).map(extractBigDecimal(key, _))
 
     override def unbind(key: String, value: BigDecimal): String = value.toString()
   }
+
+  def extractBigDecimal(key: String, value: String): Either[String, BigDecimal] =
+    Try(BigDecimal(value)).toOption match {
+      case Some(bd) => Right(bd)
+      case None => Left(s"could not convert parameter $key to a number")
+    }
 }
