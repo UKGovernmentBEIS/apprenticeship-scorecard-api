38f7158d4372ebbdc1d2c20e00cb73f1d849860f
Doug Clinton <doug@wiredthing.com>
2016-07-14 09:04:28 +0100

    Implemented the ApprenticeshipIndex

--- src/main/scala/uk/gov/bis/apprenticeshipScorecard/controllers/Apprenticeships.scala
+++ src/main/scala/uk/gov/bis/apprenticeshipScorecard/controllers/Apprenticeships.scala
@@ -5,23 +5,18 @@ import javax.inject.Inject
 import com.wellfactored.restless.play.actions.ApiActions.JsCollect
 import play.api.libs.json.{JsObject, Json}
 import play.api.mvc._
-import uk.gov.bis.apprenticeshipScorecard.models.Join
 import uk.gov.bis.apprenticeshipScorecard.tools.DataStore.ApprenticeshipWithProvider
-import uk.gov.bis.apprenticeshipScorecard.tools.{ProviderIndex, Ranked, TSVLoader}
+import uk.gov.bis.apprenticeshipScorecard.tools.{ApprenticeshipIndex, Ranked, TSVLoader}
 
 import scala.concurrent.ExecutionContext
 
 class Apprenticeships @Inject()(implicit ec: ExecutionContext) extends Controller with LocationSearchSupport {
   def apprenticeships = JsCollect(TSVLoader.dataStore.apprenticeshipsJs)
 
-  implicit class ProviderIndexSyntax(index: ProviderIndex) {
+  implicit class ApprenticeshipIndexSyntax(index: ApprenticeshipIndex) {
     def matchPhrase(op: Option[String]): Seq[Ranked[ApprenticeshipWithProvider]] = op match {
-      case None => TSVLoader.dataStore.apprenticeshipsWithProvider.toSeq.map(Ranked(_, 1))
-      case Some(phrase) => index.matchPhrase(phrase).flatMap { r =>
-        r.item.secondary.map { a =>
-          Ranked(Join(a, TSVLoader.dataStore.providers(a.provider_id), "provider"), r.rank, r.distance)
-        }
-      }
+      case None => index.all
+      case Some(phrase) => index.matchPhrase(phrase)
     }
   }
 
@@ -35,6 +30,6 @@ class Apprenticeships @Inject()(implicit ec: ExecutionContext) extends Controlle
     val lsp = for (lat <- lato; lon <- lono; dist <- disto)
       yield LocationSearchParams(Point(lat, lon), dist)
 
-    ProviderIndex.matchPhrase(phrase).searchLocation(lsp).sortBy(_.rank).map(Json.toJson(_).as[JsObject])
+    ApprenticeshipIndex.matchPhrase(phrase).searchLocation(lsp).sortBy(_.rank).map(Json.toJson(_).as[JsObject])
   }
 }
---
+++ src/main/scala/uk/gov/bis/apprenticeshipScorecard/tools/ApprenticeshipIndex.scala
@@ -0,0 +1,74 @@
+package uk.gov.bis.apprenticeshipScorecard.tools
+
+import uk.gov.bis.apprenticeshipScorecard.models.{Apprenticeship, SubjectCode}
+import uk.gov.bis.apprenticeshipScorecard.tools.DataStore.ApprenticeshipWithProvider
+
+class ApprenticeshipIndex(dataStore: DataStore) extends Index[ApprenticeshipWithProvider] {
+
+  override def all: Seq[Ranked[ApprenticeshipWithProvider]] =
+    dataStore.apprenticeshipsWithProvider.map(Ranked(_, 1)).toSeq
+
+  /**
+    * Match a single word against the index
+    *
+    * @param word the word to match
+    * @return a set of entries with a matching rank
+    */
+  override def lookup(word: String): Seq[Ranked[ApprenticeshipWithProvider]] = index.lookup(word)
+
+  lazy val index: Index[ApprenticeshipWithProvider] = {
+    val (keywordMap, codeMap) = extractMaps
+
+    new Index[ApprenticeshipWithProvider] {
+      // Not the most efficient implementation, but good enough for the 800-odd providers in our data set
+      override def lookup(word: String): Seq[Ranked[ApprenticeshipWithProvider]] = {
+        val searchWord = word.trim().toLowerCase
+        if (searchWord != "") {
+          val wordMatches = keywordMap.keys.filter(_.startsWith(searchWord)).flatMap { key =>
+            keywordMap(key).map(prn => Ranked(prn, 1.0 + searchWord.length.toDouble / key.length))
+          }
+          val codeMatches = codeMap.keys.filter(_ == searchWord).flatMap(codeMap(_).map(prn => Ranked(prn, 2.0)))
+          (wordMatches ++ codeMatches).toSeq
+        } else Seq()
+      }
+
+      override def all: Seq[Ranked[ApprenticeshipWithProvider]] = dataStore.apprenticeshipsWithProvider.map(Ranked(_, 1)).toSeq
+    }
+  }
+
+  def normalise(s: String): String = s.trim.toLowerCase
+
+  def splitWords(s: String): List[String] = s.split("\\s").toList
+
+  def extractMaps = extractWordIndices match {
+    case (keywordMaps, subjectCodeMaps) => (mergeMaps(keywordMaps), mergeMaps(subjectCodeMaps))
+  }
+
+  /**
+    * For each provider build one map that indexes it against keywords and one that indexes
+    * it against the subject codes it offers.
+    *
+    * @return a pair of maps with the keyword index as the first element and the subject code index
+    *         as the second element.
+    */
+  def extractWordIndices: (Iterable[Map[String, ApprenticeshipWithProvider]], Iterable[Map[String, ApprenticeshipWithProvider]]) =
+    dataStore.apprenticeshipsWithProvider.map { app =>
+      val (subjectCode, subjectTitle) = extractSubject(app.primary)
+      val provider = app.secondary
+
+      val keywords = splitWords(provider.name) ++ splitWords(subjectTitle).map(normalise)
+      (Map(keywords.map(k => k -> app): _*), Map(subjectCode.code.toString -> app))
+    }.unzip
+
+
+  def extractSubject(app: Apprenticeship): (SubjectCode, String) = (app.subject_tier_2_code, app.subject_tier_2_title)
+
+
+  def mergeMaps(maps: Iterable[Map[String, ApprenticeshipWithProvider]]): Map[String, Set[ApprenticeshipWithProvider]] = {
+    maps.foldLeft(Map[String, Set[ApprenticeshipWithProvider]]()) { case (alreadyMerged, stringToUkprn) =>
+      alreadyMerged ++ stringToUkprn.map { case (k, v) => k -> alreadyMerged.get(k).map(_ + v).getOrElse(Set(v)) }
+    }
+  }
+}
+
+object ApprenticeshipIndex extends ApprenticeshipIndex(TSVLoader.dataStore)
