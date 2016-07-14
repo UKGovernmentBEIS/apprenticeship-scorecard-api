8910a5496c66e2e38215d40908291f38fc88a701
Doug Clinton <doug@wiredthing.com>
2016-07-14 08:45:59 +0100

    Extracted the ProviderIndex into its own file in preparation for adding an ApprenticeshipIndex

--- src/main/scala/uk/gov/bis/apprenticeshipScorecard/tools/Index.scala
+++ src/main/scala/uk/gov/bis/apprenticeshipScorecard/tools/Index.scala
@@ -1,8 +1,5 @@
 package uk.gov.bis.apprenticeshipScorecard.tools
 
-import uk.gov.bis.apprenticeshipScorecard.models._
-import uk.gov.bis.apprenticeshipScorecard.tools.DataStore.ProviderWithApprenticeships
-
 trait Index[T] {
   /**
     * Match a single word against the index
@@ -36,73 +33,4 @@ case class Ranked[T](item: T, rank: Double, distance: Option[Double] = None) {
   def addRank(r: Double): Ranked[T] = copy(rank = this.rank + r)
 
   def withDistance(d: Double): Ranked[T] = copy(distance = Some(d))
-}
-
-object ProviderIndex extends ProviderIndex(TSVLoader.dataStore)
-
-class ProviderIndex(dataStore: DataStore) extends Index[ProviderWithApprenticeships] {
-  override def lookup(s: String): Seq[Ranked[ProviderWithApprenticeships]] = for {
-    rankedPrn <- index.lookup(s)
-    provider <- dataStore.providersWithApprenticeships.find(_.primary.ukprn == rankedPrn.item)
-  } yield Ranked(provider, rankedPrn.rank)
-
-  override def all: Seq[Ranked[ProviderWithApprenticeships]] =
-    dataStore.providersWithApprenticeships.map(Ranked(_, 1)).toSeq
-
-  lazy val index: Index[UKPRN] = {
-    val (keywordMap, codeMap) = extractMaps
-
-    new Index[UKPRN] {
-      // Not the most efficient implementation, but good enough for the 800-odd providers in our data set
-      override def lookup(word: String): Seq[Ranked[UKPRN]] = {
-        val searchWord = word.trim().toLowerCase
-        if (searchWord != "") {
-          val wordMatches = keywordMap.keys.filter(_.startsWith(searchWord)).flatMap { key =>
-            keywordMap(key).map(prn => Ranked(prn, 1.0 + searchWord.length.toDouble / key.length))
-          }
-          val codeMatches = codeMap.keys.filter(_ == searchWord).flatMap(codeMap(_).map(prn => Ranked(prn, 2.0)))
-          (wordMatches ++ codeMatches).toSeq
-        } else Seq()
-      }
-
-      override def all: Seq[Ranked[UKPRN]] = dataStore.providers.keys.map(Ranked(_, 1)).toSeq
-    }
-  }
-
-  def normalise(s: String): String = s.trim.toLowerCase
-
-  def splitWords(s: String): List[String] = s.split("\\s").toList
-
-  def extractMaps = extractWordIndices match {
-    case (keywordMaps, subjectCodeMaps) => (mergeMaps(keywordMaps), mergeMaps(subjectCodeMaps))
-  }
-
-  /**
-    * For each provider build one map that indexes it against keywords and one that indexes
-    * it against the subject codes it offers.
-    *
-    * @return a pair of maps with the keyword index as the first element and the subject code index
-    *         as the second element.
-    */
-  def extractWordIndices: (Iterable[Map[String, UKPRN]], Iterable[Map[String, UKPRN]]) =
-    dataStore.providersWithApprenticeships.map { join =>
-      val (subjectCodes, subjectTitles) = extractSubjects(join)
-      val provider = join.primary
-
-      val keywords = (splitWords(provider.name) ++ subjectTitles.flatMap(splitWords)).map(normalise)
-      (Map(keywords.map(k => k -> provider.ukprn): _*), Map(subjectCodes.map(sc => sc.code.toString -> provider.ukprn): _*))
-    }.unzip
-
-
-  def extractSubjects(provider: ProviderWithApprenticeships): (List[SubjectCode], List[String]) = {
-    provider.secondary.map { a =>
-      (a.subject_tier_2_code, a.subject_tier_2_title)
-    }.toList.distinct.unzip
-  }
-
-  def mergeMaps(maps: Iterable[Map[String, UKPRN]]): Map[String, Set[UKPRN]] = {
-    maps.foldLeft(Map[String, Set[UKPRN]]()) { case (alreadyMerged, stringToUkprn) =>
-      alreadyMerged ++ stringToUkprn.map { case (k, v) => k -> alreadyMerged.get(k).map(_ + v).getOrElse(Set(v)) }
-    }
-  }
 }
